f62346a777fe1c2c64dd41a7a2b3120fba41cdd2
Marcelo Sosa <sosamarcelo92@gmail.com>
2022-07-16 04:05:22 -0400

    Nearly finished mobile ver

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Maven Pro", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        subtitle: "1.75rem",
+        title: "2rem",
+      },
+      colors: {
+        yellow: "#ffc200",
+        darkGray: "#474559",
+        textGray: "#5b5b5b;",
+        titleBlack: "#353535",
+        headerBlack: "#212121;",
+        placeHolder: "#495057",
+        lightGray: "#eee",
+        linksGray: "rgba(0,0,0,.5)",
+        inputBorder: "#ced4da",
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
